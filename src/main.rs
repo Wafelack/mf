e3617bdd27ee1d213dc3412b571dddb4bbc2858b
wafelack <wafelack@protonmail.com>
2021-06-08 15:27:17 +0200

    matcher: Add --perms.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,90 @@ use errors::Result;
 use libc::{c_char, getgrnam, getpwnam};
 use matcher::FileMatcher;
 use pattern::Pattern;
-use std::{
-    ffi::CString,
-    process::{exit, Command},
-};
+use std::process::{exit, Command};
 
 const NAME: &str = env!("CARGO_PKG_NAME");
+macro_rules! gen_app {
+    () => {
+        App::new(NAME)
+            .about("Find files")
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .version(env!("CARGO_PKG_VERSION"))
+            .arg(
+                Arg::with_name("dir")
+                .index(1)
+                .help("The directory to search in."),
+                )
+            .arg(
+                Arg::with_name("name")
+                .short("n")
+                .long("name")
+                .multiple(true)
+                .value_name("pat")
+                .takes_value(true)
+                .help("File name matches pattern(s) `pat'."),
+                )
+            .arg(
+                Arg::with_name("type")
+                .short("t")
+                .long("type")
+                .value_name("t")
+                .help("File is of type `t'. `f' stands for file and `d' for directory."),
+                )
+            .arg(
+                Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .multiple(true)
+                .takes_value(true)
+                .value_name("pat")
+                .help("File path matches pattern(s) `pat'."),
+                )
+            .arg(
+                Arg::with_name("gid")
+                .short("G")
+                .long("gid")
+                .value_name("id")
+                .help("File owner belongs to the group that has GID `id'."),
+                )
+            .arg(
+                Arg::with_name("uid")
+                .short("U")
+                .long("uid")
+                .value_name("id")
+                .help("File owner has UID `id'."),
+                )
+            .arg(
+                Arg::with_name("user")
+                .short("u")
+                .long("user")
+                .value_name("name")
+                .help("File owner has username `name'."),
+                )
+            .arg(
+                Arg::with_name("group")
+                .short("g")
+                .long("group")
+                .value_name("name")
+                .help("File owner belongs to the group named `name'."),
+                )
+            .arg(Arg::with_name("execute")
+                 .short("x")
+                 .long("exec")
+                 .value_name("command")
+                 .help("The command to run for each file. `{}' is replaced by the file name in the command."))
+            .arg(Arg::with_name("permissions")
+                 .short("P")
+                 .long("perms")
+                 .value_name("bits")
+                 .help("File has premissions bits set to `bits'."))
+            .after_help(
+                "Patterns work with wildcards, a wildcard matches every set of characters.
+For example, `*.rs` will match all the files ending in .rs.",
+)
+
+    }
+}
 
 fn main() {
     match try_main() {
@@ -24,79 +102,7 @@ fn main() {
 }
 
 fn try_main() -> Result<()> {
-    let matches = App::new(NAME)
-        .about("Find files")
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .arg(
-            Arg::with_name("dir")
-            .index(1)
-            .help("The directory to search in."),
-            )
-        .arg(
-            Arg::with_name("name")
-            .short("n")
-            .long("name")
-            .multiple(true)
-            .value_name("pat")
-            .takes_value(true)
-            .help("File name matches pattern(s) `pat'."),
-            )
-        .arg(
-            Arg::with_name("type")
-            .short("t")
-            .long("type")
-            .value_name("t")
-            .help("File is of type `t'. `f' stands for file and `d' for directory."),
-            )
-        .arg(
-            Arg::with_name("path")
-            .short("p")
-            .long("path")
-            .multiple(true)
-            .takes_value(true)
-            .value_name("pat")
-            .help("File path matches pattern(s) `pat'."),
-            )
-        .arg(
-            Arg::with_name("gid")
-            .short("G")
-            .long("gid")
-            .value_name("id")
-            .help("File owner belongs to the group that has GID `id'."),
-            )
-        .arg(
-            Arg::with_name("uid")
-            .short("U")
-            .long("uid")
-            .value_name("id")
-            .help("File owner has UID `id'."),
-            )
-        .arg(
-            Arg::with_name("user")
-            .short("u")
-            .long("user")
-            .value_name("name")
-            .help("File owner has username `name'."),
-            )
-        .arg(
-            Arg::with_name("group")
-            .short("g")
-            .long("group")
-            .value_name("name")
-            .help("File owner belongs to the group named `name'."),
-            )
-        .arg(Arg::with_name("execute")
-             .short("x")
-             .long("exec")
-             .value_name("command")
-             .help("The command to run for each file. `{}' is replaced by the file name in the command."))
-        .after_help(
-            "Patterns work with wildcards, a wildcard matches every set of characters.
-For example, `*.rs` will match all the files ending in .rs.",
-)
-        .get_matches();
-
+    let matches = gen_app!().get_matches();
     let dir = matches.value_of("dir").unwrap_or(".");
     let name = matches
         .values_of("name")
@@ -147,7 +153,10 @@ For example, `*.rs` will match all the files ending in .rs.",
             None => Ok(None),
         },
     }?;
-
+    let perms = matches.value_of("perms").map_or(Ok(None), |v| match u32::from_str_radix(v, 8) {
+        Ok(v) => Ok(Some(v)),
+        Err(_) => error!("Invalid permission bits: `{}'", v),
+    })?;
     let mut matcher = FileMatcher::from_dir(dir)?;
     matcher.set_ftype(ftype.map_or(Ok(None), |t| {
         if t == "f" {
@@ -162,6 +171,7 @@ For example, `*.rs` will match all the files ending in .rs.",
     matcher.add_ppatterns(&path);
     matcher.set_uid(uid);
     matcher.set_gid(gid);
+    matcher.set_perms(perms);
     let matched = matcher.matches();
     match matches.value_of("execute") {
         Some(v) => {
@@ -186,12 +196,12 @@ For example, `*.rs` will match all the files ending in .rs.",
                         Err(_) => error!(
                             "Failed to summon command: `{}`",
                             replaced_args.fold(cmd.to_string(), |acc, s| format!("{} {}", acc, s))
-                        ),
+                            ),
                     }?;
 
                     Ok(status.code().unwrap_or(-1))
                 })
-                .collect::<Result<Vec<i32>>>()?;
+            .collect::<Result<Vec<i32>>>()?;
             for (idx, code) in codes.into_iter().enumerate() {
                 if code != 0 {
                     return error!("Command #{} failed, exit code: {}.", idx, code);
@@ -220,19 +230,23 @@ fn to_args<'a>(s: &'a str) -> Vec<(u8, &'a str)> {
                             vec![(1, s)]
                         }
                     })
-                    .flatten()
+                .flatten()
                     .filter(|(_, s)| !s.is_empty())
                     .collect()
             } else {
                 vec![(0, s)]
             }
         })
-        .flatten()
+    .flatten()
         .filter(|(_, s)| !s.is_empty())
         .collect()
 }
 
 fn to_cchar(s: &str) -> *const c_char {
-    let cstring = CString::new(s).unwrap();
-    cstring.as_ptr()
+    let mut bytes = s.chars().map(|c| {
+        println!("{};{}", c, c as i8);
+        c as i8
+    }).collect::<Vec<i8>>();
+    bytes.push(0);
+    bytes.as_ptr()
 }
--- src/matcher.rs
+++ src/matcher.rs
@@ -9,20 +9,22 @@ pub struct File {
     ftype: bool, /* True for directory */
     uid: u32,
     gid: u32,
+    perms: u32,
     pub path: String,
 }
 impl File {
-    pub fn new(path: String, ftype: bool, uid: u32, gid: u32) -> Self {
+    pub fn new(path: String, ftype: bool, uid: u32, gid: u32, perms: u32) -> Self {
         Self {
             name: path
                 .split('/')
                 .last()
                 .and_then(|s| Some(s.to_string()))
                 .unwrap(),
-            ftype,
-            uid,
-            gid,
-            path,
+                ftype,
+                uid,
+                gid,
+                path,
+                perms,
         }
     }
 }
@@ -33,6 +35,7 @@ pub struct FileMatcher {
     ppatterns: Vec<Pattern>,
     gid: Option<u32>,
     uid: Option<u32>,
+    perms: Option<u32>,
     ftype: Option<char>,
 }
 impl FileMatcher {
@@ -43,6 +46,7 @@ impl FileMatcher {
             ppatterns: vec![],
             gid: None,
             uid: None,
+            perms: None,
             ftype: None,
         })
     }
@@ -61,6 +65,9 @@ impl FileMatcher {
     pub fn set_uid(&mut self, id: Option<u32>) {
         self.uid = id;
     }
+    pub fn set_perms(&mut self, perms: Option<u32>) {
+        self.perms = perms;
+    }
     pub fn matches(&self) -> Vec<File> {
         self.files
             .clone()
@@ -76,6 +83,7 @@ impl FileMatcher {
                     .fold(true, |acc, p| acc && p.matches(f.path.clone()));
                 let gid = self.gid.map_or(true, |i| i == f.gid);
                 let uid = self.uid.map_or(true, |i| i == f.uid);
+                let mode = self.perms.map_or(true, |m| m == f.perms);
                 let ftype = if let Some(c) = self.ftype {
                     if c == 'f' && f.ftype {
                         false
@@ -87,35 +95,36 @@ impl FileMatcher {
                 } else {
                     true
                 };
-                if name && path && ftype && gid && uid {
+                if name && path && ftype && gid && uid && mode {
                     Some(f)
                 } else {
                     None
                 }
             })
-            .filter(|f| f.is_some())
+        .filter(|f| f.is_some())
             .map(|f| f.unwrap())
             .collect()
     }
 }
+const MODE_MASK: u32 = 0b111111111111;
 fn get_files(dir: String) -> Result<Vec<File>> {
     Ok(fs::read_dir(dir)?
-        .map(|e| {
-            let entry = e?;
-            let path = entry.path();
-            let md = fs::metadata(&path)?;
-            let stringified = path.to_str().unwrap().to_string();
-            let f = File::new(stringified.clone(), path.is_dir(), md.uid(), md.gid());
-            Ok(if path.is_dir() {
-                let mut files = vec![f];
-                files.extend(get_files(stringified)?);
-                files
-            } else {
-                vec![f]
-            })
-        })
-        .collect::<Result<Vec<Vec<File>>>>()?
-        .into_iter()
-        .flatten()
-        .collect())
+       .map(|e| {
+           let entry = e?;
+           let path = entry.path();
+           let md = fs::metadata(&path)?;
+           let stringified = path.to_str().unwrap().to_string();
+           let f = File::new(stringified.clone(), path.is_dir(), md.uid(), md.gid(), md.mode() & MODE_MASK);
+           Ok(if path.is_dir() {
+               let mut files = vec![f];
+               files.extend(get_files(stringified)?);
+               files
+           } else {
+               vec![f]
+           })
+       })
+       .collect::<Result<Vec<Vec<File>>>>()?
+       .into_iter()
+       .flatten()
+       .collect())
 }
